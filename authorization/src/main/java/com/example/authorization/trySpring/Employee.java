195f70677acc7cd97f9528d68defa91c3cc37314
sazzadulalambd <sazzad.sua@gmail.com>
2024-04-03 16:16:33 +0600

    update dependency

---
+++ authorization/src/main/java/com/example/authorization/trySpring/Employee.java
@@ -0,0 +1,11 @@
+package com.example.authorization.trySpring;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private int employeeId;
+    private String employeeName;
+    private int employeeAge;
+    private String employeeDepartment;
+}
---
+++ authorization/src/main/java/com/example/authorization/trySpring/HomeRepository.java
@@ -0,0 +1,44 @@
+package com.example.authorization.trySpring;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HomeRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Map<String, Object> findOne(int id) {
+        String query = "SELECT id, name, age, department FROM employee WHERE id=?";
+        return jdbcTemplate.queryForMap(query, id);
+    }
+
+
+}
+
+//private final JdbcTemplate jdbcTemplate;
+//
+//@Autowired
+//public HomeRepository(JdbcTemplate jdbcTemplate) {
+//    this.jdbcTemplate = jdbcTemplate;
+//}
+
+//
+//public Map<String, Object> findOne(int id) {
+//    // SELECT statement
+//    String query = "SELECT "
+//            + "id,"
+//            + "name,"
+//            + "age,"   // Added missing comma here
+//            + "department "
+//            + "FROM employee "
+//            + "WHERE id=?";
+//
+//    // Executing the query and fetching the result
+//    Map<String, Object> employee = jdbcTemplate.queryForMap(query, id);
+//
+//    return employee;
+//}
